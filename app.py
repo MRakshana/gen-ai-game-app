d075101a2f09b2381273d676d12d9d95ea99ec3e
Rakshana M <147284193+MRakshana@users.noreply.github.com>
2025-05-11 10:48:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ CLUE_QUESTIONS = {
     "Is it an object?": lambda word: word not in ["elephant", "tiger", "apple"],
     "Is it used in school?": lambda word: word in ["chair", "pencil"],
     "Is it a musical instrument?": lambda word: word == "guitar",
-    "Is it edible?": lambda word in ["apple", "pizza"],
-    "Does it have four legs?": lambda word in ["elephant", "tiger", "chair"],
+    "Is it edible?": lambda word: word in ["apple", "pizza"],
+    "Does it have four legs?": lambda word: word in ["elephant", "tiger", "chair"],
     "Can it fly?": lambda word == "rocket"
 }
 
