2b7cbcfc5b5a4c1ba4614881e9c75222ec913698
Rakshana M <147284193+MRakshana@users.noreply.github.com>
2025-05-12 22:11:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import sys
 import streamlit as st
 from langgraph.graph import StateGraph, END
-
+from typing import TypedDict, Optional
 # Optionally increase recursion limit
 sys.setrecursionlimit(1000)
 
